11108e333240f1f842bb4f14b835036e7b426579
Omar Hassan <AverageOmar@users.noreply.github.com>
2016-10-05 12:25:26 +0100

    Add files via upload

--- main.lua
+++ main.lua
@@ -11,12 +11,16 @@ function love.load()
   background2= love.graphics.newImage("sprites/bg.png")
   backgroundQuad = love.graphics.newQuad(1,1,720/2,1280/2,720/2,1280/2)
   background1X = 0
-  background2X = 720
+  background2X = 360
   
   flappy = love.graphics.newImage("sprites/flappy.png")
   flappyPosY = 200
   
   ground = love.graphics.newImage("sprites/ground.png")
+  ground2 = love.graphics.newImage("sprites/ground.png")
+  groundQuad = love.graphics.newQuad(1, 1, 360, 640, 360, 281)
+  groundX = 0
+  ground2X = 360
   
   flappyDead = love.graphics.newImage("sprites/flappydead.png")
   
@@ -30,7 +34,8 @@ function love.load()
 end
 
 function love.draw() -- draw screen
-  love.graphics.draw(background, backgroundQuad, 0, 0)
+  love.graphics.draw(background, backgroundQuad, background1X, 0)
+  love.graphics.draw(background2, backgroundQuad, background2X, 0)
   game_screen()
 end
 
@@ -38,6 +43,8 @@ function reset() -- reset game
   flappyPosY = 200
   alive = true
   jump = 2.5
+  rotate = 0
+  score = 0
   pipePosX = 300
   pipe2PosX = pipePosX
 end
@@ -48,7 +55,6 @@ function flappyJump() --set the jump speed
 end
 
 function love.keypressed(key) --keyboard input
-  
   --set jump or reset
   if key == "space" and alive == true then
     flappyJump()
@@ -61,28 +67,54 @@ end
 
 function game_screen()
   --create character image variables
-  love.graphics.draw(pipe, pipePosX, 500)
+  love.graphics.draw(pipe, pipePosX, 350)
+  love.graphics.draw(pipe2, pipe2PosX, 0)
+  
+  --draw the floor
+  love.graphics.draw(ground, groundQuad, groundX, 500)
+  love.graphics.draw(ground2, groundQuad, ground2X, 500)
   
   if alive == true then
     love.graphics.draw(flappy, 125, flappyPosY, math.rad(rotate), 1, 1, 22.5, 22.5)
   end
   if alive == false then
-    love.graphics.draw(flappyDead, 125, flappyPosY)
+    love.graphics.draw(flappyDead, 125, flappyPosY, math.rad(rotate), 1, 1, 22.5, 22.5)
   end
-  
-  love.graphics.draw(pipe2, pipe2PosX, 0)
   
   if alive == false then
     love.graphics.draw(gameOver, 90, 250)
+    love.graphics.print( "You scored "..score.." points", 120, 320)
   end
   
-  --love.graphics.draw(ground, 0, 500)
+  if alive == true then
+    --moving floor
+    groundX = groundX - 1.5
+    ground2X = ground2X - 1.5
   
-  --write text
-  love.graphics.print( "score is: "..score, 0, 0)
+    if groundX < -359 then
+      groundX = 360
+    end
+    if ground2X < -359 then
+      ground2X = 360
+    end
+  
+    --moving background
+    background1X = background1X - 1.5
+    background2X = background2X - 1.5
+  
+    if background1X < -359 then
+      background1X = 360
+    end
+    if background2X < -359 then
+      background2X = 360
+    end
+  end
   
   --set pipe placement and movement
   if alive == true then
+    --write text
+    love.graphics.print( "score is: "..score, 0, 0)
+    
     pipePosX = pipePosX - 1.5
     pipe2PosX = pipe2PosX - 1.5
   end
@@ -90,7 +122,7 @@ function game_screen()
   --jump and movement
   flappyPosY = flappyPosY + jump
   
-  if flappyPosY < 600 then
+  if flappyPosY < 700 then
     if jump < 15 then
       jump = jump + 0.2
     end
@@ -102,17 +134,33 @@ function game_screen()
   end
   
   --collision and bounding boxes
-  hitTest = CheckCollision(pipePosX, 500, 60, 150, 125, flappyPosY, 45, 45)
+  hitTest = CheckCollision(125, flappyPosY, 45, 45, groundX, 550, 360, 281)
+  if(hitTest) then
+    alive = false
+    jump = 0
+  end
+  
+  hitTest = CheckCollision(125, flappyPosY, 45, 45, ground2X, 550, 360, 281)
+  if(hitTest) then
+    alive = false
+    jump = 0
+  end
+  
+  hitTest = CheckCollision(pipePosX, 350, 60, 150, 125, flappyPosY, 22.5, 22.5)
   if(hitTest) then
     alive = false
   end
   
-  hitTest = CheckCollision(pipe2PosX, 0, 60, 150, 125, flappyPosY, 45, 45)
+  hitTest = CheckCollision(pipe2PosX, 0, 60, 150, 125, flappyPosY, 22.5, 22.5)
   if(hitTest) then
     alive = false
   end
   
-  if flappyPosY > 600 or flappyPosY < 0 then
+  if flappyPosY < -22 then
+    alive = false
+  end
+  
+  if flappyPosY > 600 then
     alive = false
     jump = 0
   end
